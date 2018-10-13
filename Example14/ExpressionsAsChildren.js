4dc6573f8bb9634feb017f58bea8b56241f7a062
Michael Anthony Ballard <MichaelBallard@Michaels-MacBook-Pro-4.local>
2018-10-13 18:46:12 -0400

    added and example

---
+++ Example14/ExpressionsAsChildren.js
@@ -0,0 +1,17 @@
+function ToDo(props){
+    return <div> ToDo: {props.item}</div>
+}
+
+function List(props){
+    var list = ['clean car', 'finish email', 'get groceries'];
+    return(
+        // create a jsx expression to map over an array of elements
+        <ul>
+            {/* when I have similar elements display sequentially they must have a key, react requires unique identifiers */}
+            {list.map((todo)=> <ToDo key={todo} item={todo} /> )}
+        </ul>
+    )
+}
+
+ReactDOM.render(<List/>, 
+    document.getElementById('root'))
